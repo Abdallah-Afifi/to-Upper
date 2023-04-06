8ff221fcf0d5da6706c864fb1519b99ad6f7877a
AbdallahFathi22 <114703186+AbdallahFathi22@users.noreply.github.com>
2023-04-06 10:13:23 +0200

    Add files via upload

---
+++ Ass1.Prob1.cpp
@@ -0,0 +1,27 @@
+/*
+Abdallah Afifi
+900225283
+Problem 1
+*/
+
+#include <iostream>
+using namespace std;
+
+char uppercase(char ch) {
+    if (ch >= 'a' && ch <= 'z') { // checking if ch is a lowercase letter
+        return ch - 32; // subtract 32 from the ASCII code of ch to convert it to uppercase
+    }
+    else { // ch is already uppercase or not a letter
+        return ch; // return ch as is
+    }
+}
+
+int main() {
+    char c;
+    cout << "Enter a character: ";
+    cin >> c;
+    cout << "The uppercase is " << uppercase(c) << endl;
+    return 0;
+}
+
+
